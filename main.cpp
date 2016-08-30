ed42a3cc1075a197f98effb08f2e171ecf1bca73
fengwutianya <949979029@qq.com>
2016-08-30 10:41:14 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your first name: ";
+    string name;
+    cin>> name;
+
+    const string greeting = "Hello, " + name + "!";
+    const int pad = 5;
+    const int rows = pad * 2 + 3;
+    const string::size_type cols = greeting.size() + pad*2 + 2;
+
+
+    for (int r = 0; r < rows; ++r) {
+        string::size_type c = 0;
+        while (c != cols) {
+            if (r == 0 || r == rows -1 || c == 0 || c == cols - 1) {
+                cout << "*";
+                ++c;
+            } else {
+                if (r == pad + 1 && c == pad + 1) {
+                    cout << greeting;
+                    c += greeting.size();
+                } else {
+                    cout << " ";
+                    ++c;
+                }
+            }
+        }
+        cout << endl;
+
+    }
+
+    return 0;
+}
