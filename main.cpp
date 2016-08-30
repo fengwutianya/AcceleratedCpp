dbf02af3d568606d469b4d95e4523a313c12e046
fengwutianya <949979029@qq.com>
2016-08-30 21:43:00 +0800

    acceleratedcpp 3

--- main.cpp
+++ main.cpp
@@ -1,38 +1,49 @@
+#include <iomanip>
+#include <ios>
 #include <iostream>
+#include <String>
+#include <vector>
+#include <algorithm>
+
+
 
 using namespace std;
 
 int main()
 {
+    typedef vector<double>::size_type vec_sz;
     cout << "Please enter your first name: ";
     string name;
-    cin>> name;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
 
-    const string greeting = "Hello, " + name + "!";
-    const int pad = 5;
-    const int rows = pad * 2 + 3;
-    const string::size_type cols = greeting.size() + pad*2 + 2;
+    cout << "Please enter your midterm and final grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
 
+    cout << "Enter all your homework grades, "
+            "followed by end_of_file: ";
 
-    for (int r = 0; r < rows; ++r) {
-        string::size_type c = 0;
-        while (c != cols) {
-            if (r == 0 || r == rows -1 || c == 0 || c == cols - 1) {
-                cout << "*";
-                ++c;
-            } else {
-                if (r == pad + 1 && c == pad + 1) {
-                    cout << greeting;
-                    c += greeting.size();
-                } else {
-                    cout << " ";
-                    ++c;
-                }
-            }
-        }
-        cout << endl;
-
+    vector<double> homework;
+    double x;
+    while (cin >> x) {
+        homework.push_back(x);
     }
+    sort(homework.begin(), homework.end());
+    vec_sz size = homework.size();
+    if (size == 0) {
+        cout << endl << "You must enter your homework grades."
+                    << "Please try again. " << endl;
+        return 1;
+    }
+    streamsize prec = cout.precision();
+    vec_sz mid = size/2;
+    double median;
+    median = size % 2 == 0 ? homework[mid] / 2 + homework[mid+1] / 2 :
+             homework[mid];
+    cout << "Your final grade is " << setprecision(3)
+            << 0.2 * midterm + 0.4 * final + 0.4 * median
+            <<setprecision(prec) << endl;
 
     return 0;
 }
